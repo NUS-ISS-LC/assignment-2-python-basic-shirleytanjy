f7b37d86b00a62551c51e763fd0f06c592b49a05
Shirley Tan Jing Yi <e0235128@u.nus.edu>
2025-11-08 03:11:58 +0000

    submit assignment

--- ex1.py
+++ ex1.py
@@ -1,3 +1,15 @@
+nums = [2,7,11,15]
+target = 9
+
 def find(s, n):
 # write your implementation here
+    for i in range(len(nums)):
+        for j in range (i+1, len(nums)):
+            if ((nums[i]+nums[j]) == target):
+                return [i,j]
     return None
+
+# to test method
+result = find(nums, target)
+print("Test: {} | Target: {}".format(nums,target))
+print("Result: {}".format(result))
